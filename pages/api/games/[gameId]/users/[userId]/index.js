086d5f620b9de4911d62d1507ddfeb5869118cee
Pablo Yncio Rivera <pabloynciorivera@gmail.com>
2021-11-16 10:02:50 -0500

    refact cors

--- pages/api/games/[gameId]/users/[userId]/index.js
+++ pages/api/games/[gameId]/users/[userId]/index.js
@@ -1,14 +1,12 @@
-import postGame from "../../../../../../src/api/games/_gameId/users/_userId/postGame";
-import putGame from "../../../../../../src/api/games/_gameId/users/_userId/putGame";
 import Cors from "cors";
 import initMiddleware from "../../../../../../lib";
+import postGame from "../../../../../../src/api/games/_gameId/users/_userId/postGame";
+import putGame from "../../../../../../src/api/games/_gameId/users/_userId/putGame";
 
-// Initialize the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
 const cors = initMiddleware(
-  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
   Cors({
-    // Only allow requests with GET, POST and OPTIONS
-    methods: ["GET", "POST", "OPTIONS"]
+    methods: ["GET", "POST", "DELETE"],
   })
 );
 
