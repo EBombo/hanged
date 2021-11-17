cadd685398783809f911a56099976544794cb182
anargu <anargu@pop-os.localdomain>
2021-11-17 18:33:15 -0500

    wip alphabet and timer

--- src/pages/lobbies/_lobbyId/play/Alphabet.js
+++ src/pages/lobbies/_lobbyId/play/Alphabet.js
@@ -1,13 +1,46 @@
 import React from "reactn";
 import styled from "styled-components";
+import { alphabet } from "../../../../components/common/DataList";
+import { ButtonAnt } from "../../../../components/form";
+
+const getColorByLetterState = (lettersPressed, letter) => {
+  if (lettersPressed[letter] === "unmatched") return "default";
+  if (lettersPressed[letter] === "matched") return "success";
+  return "primary";
+};
 
 export const Alphabet = (props) => (
   <AlphabetContainer>
+    { alphabet.map((letter, i) => 
+      <ButtonAnt
+        key={`letter-key-${i}`}
+        color={getColorByLetterState(props.lettersPressed, letter)}
+        disabled={getColorByLetterState(props.lettersPressed, letter) === "default" ? true : false}
+        className="letter"
+        onClick={() => props.onLetterPressed?.(letter)}
+      >{letter}</ButtonAnt>
+    )}
   </AlphabetContainer>
 );
 
 const AlphabetContainer = styled.div`
-  display: inline-block;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+
+  .letter {
+    margin: 0.3rem;
+    padding: 6px 6px;
+    display: inline-flex;
+    align-items: center;
+    justify-content: center;
+    border-radius: 4px;
+    min-width: 38px;
+    min-height: 38px;
+    font-weight: bold;
+    font-family: Lato;
+    font-style: normal;
+  }
 `;
 
 
