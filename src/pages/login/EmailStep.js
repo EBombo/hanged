fd4b3830e662951fb7c2bc08f8b6ac1fd31f575f
sebastianmendo1995 <sebastian.mendo1995@gmail.com>
2021-11-18 17:50:05 -0500

    refactoring user login, adding avatar

--- src/pages/login/EmailStep.js
+++ src/pages/login/EmailStep.js
@@ -1,4 +1,4 @@
-import React, { useState } from "reactn";
+import React, { useGlobal, useState } from "reactn";
 import styled from "styled-components";
 import { Image } from "../../components/common/Image";
 import { config } from "../../firebase";
@@ -6,8 +6,12 @@ import { ButtonLobby, InputBingo } from "../../components/form";
 import { object, string } from "yup";
 import { useForm } from "react-hook-form";
 import { ModalVerification } from "./ModalVerification";
+import { useUser } from "../../hooks";
 
 export const EmailStep = (props) => {
+  const [, setAuthUserLs] = useUser();
+  const [authUser, setAuthUser] = useGlobal("user");
+
   const [email, setEmail] = useState(null);
 
   const validationSchema = object().shape({
@@ -27,24 +31,31 @@ export const EmailStep = (props) => {
         <ModalVerification
           email={email}
           isVisibleModalVerification={!!email}
-          setIsVisibleModalVerification={(email) => props.setEmailVerification(email)}
+          setIsVisibleModalVerification={async (email) => {
+            await setAuthUser({ ...authUser, email: email });
+            setAuthUserLs({ ...authUser, email: email });
+          }}
           {...props}
         />
       )}
+
       <Image src={`${config.storageUrl}/resources/white-icon-ebombo.png`} width="180px" margin="10px auto" />
+
       <div className="login-container">
         <div className="subtitle">Añadir identificación del jugador</div>
         <div className="description">
           El anfitrión del juego ha pedido que coloques tu mail cómo una medida de identificación para entrar al juego
         </div>
+
         <InputBingo
           ref={register}
           error={errors.email}
           name="email"
           align="center"
           width="100%"
-          margin="10px auto"
           variant="default"
+          margin="10px auto"
+          defaultValue={authUser?.email ?? null}
           disabled={props.isLoading}
           placeholder="Ingresa tu email"
         />
