51379eeaf9e9f4aa0089cf56b694b18332c1f59b
anargu <anargu@pop-os.localdomain>
2021-11-18 18:27:47 -0500

    updated lobbyInPlay

--- src/pages/lobbies/_lobbyId/play/LobbyInPlay.js
+++ src/pages/lobbies/_lobbyId/play/LobbyInPlay.js
@@ -7,13 +7,35 @@ import { firestore } from "../../../../firebase";
 import { HangedMan } from "./HangedMan";
 import { Timer } from "./Timer";
 import { Alphabet } from "./Alphabet";
+import { OverlayResult } from "./OverlayResult";
 import { ButtonAnt } from "../../../../components/form";
+import { GameSettings } from "./GameSettings";
+import { useInterval } from "../../../../hooks/useInterval";
+
+const orderLimbs = ["head", "leftLeg", "rightLeg", "leftArm", "rightArm", "trunk"];
+const defaultHandMan = {
+  head: 'hidden',
+  leftLeg: 'hidden',
+  rightLeg: 'hidden',
+  leftArm: 'hidden',
+  rightArm: 'hidden',
+  trunk: 'hidden',
+};
 
 export const LobbyInPlay = (props) => {
   const [authUser] = useGlobal("user");
   const [user, setUser] = useState(null);
-
-  const [currentPhrase, setCurrentPhrase] = useState("hola");
+  const [game, setGame] = useState({
+    ...props.lobby.game,
+    hangedMan: { ...defaultHandMan },
+    lettersPressed: {},
+    lives: orderLimbs.length,
+  });
+  const [currentPhraseIndex, setCurrentPhraseIndex] = useState(0);
+  const [secondsLeft, setSecondsLeft] = useState(-1);
+  const [isRoundOver, setIsRoundOver] = useState(false);
+  const [hasGuessed, setHasGuessed] = useState(false);
+  const [gameMenuEnabled, setGameMenuEnabled] = useState(false);
 
   useEffect(() => {
     const currentUserId = authUser.id;
@@ -22,6 +44,82 @@ export const LobbyInPlay = (props) => {
     props.logout();
   }, [props.lobby.users]);
 
+  // TODO: consider refactor time interval
+  const timeCountdown = () => {
+      if (secondsLeft === 0 || isRoundOver) {
+        setIsRoundOver(true);
+        return;
+      }
+      setSecondsLeft(secondsLeft - 1);
+  };
+  useInterval(timeCountdown, 1000);
+  useEffect(() => {
+    setSecondsLeft(props.lobby.game.secondsPerRound);
+  }, [currentPhraseIndex]);
+
+  const onNewLetterPressed = (letter) => {
+    if (game.lives === 0) return;
+
+    const isMatched = game.phrases[currentPhraseIndex].toUpperCase().includes(letter);
+    let livesLeft = game.lives;
+    let hangedMan = game.hangedMan;
+    if (!isMatched) {
+      ({ livesLeft, hangedMan } = penalize());
+    }
+    if (game.phrases[currentPhraseIndex].split('').filter((letter) => Object.keys(game.lettersPressed).includes(letter)).length === game.phrases[currentPhraseIndex].length) {
+      setHasGuessed(true);
+      setIsRoundOver(true);
+    }
+    
+    setGame({
+      ...game,
+      lives: livesLeft,
+      hangedMan: hangedMan,
+      lettersPressed: { 
+        ...game.lettersPressed,
+        [letter]: isMatched ? "matched" : "unmatched",
+      }
+    });
+  };
+
+  const penalize = () => {
+    const indexLimb = game.lives - 1;
+    const livesLeft = game.lives - 1;
+    if (livesLeft === 0) setIsRoundOver(true);
+    const hangedManUpdated = {...game.hangedMan, [orderLimbs[indexLimb]]: "active"};
+
+    return { livesLeft, hangedMan: hangedManUpdated};
+  };
+
+  const nextRound = () => {
+    setCurrentPhraseIndex((prev) => prev + 1);
+    resetRound();
+  };
+
+  const resetRound = () => {
+    setIsRoundOver(false);
+    setHasGuessed(false);
+    setGame({
+      ...game,
+      lettersPressed: {},
+      hangedMan: { ...defaultHandMan },
+      lives: orderLimbs.length
+    });
+  };
+
+  const resetGame = () => {
+    setCurrentPhraseIndex(0);
+    resetRound();
+  };
+
+  const isGameOver = () => {
+    return (game.phrases.length <= (currentPhraseIndex + 1));
+  };
+
+  const updateGameAndRestart = async (updatedGame) => {
+    resetGame();
+    setGame({...updatedGame});
+  };
 
   // TODO: Consider to refactoring, <Admin> & <User>.
   return (
@@ -29,260 +127,97 @@ export const LobbyInPlay = (props) => {
       <UserLayout {...props} />
 
       <HangedGameContainer>
-        <Timer {...props}/>
-        <HangedMan {...props}/>
-        <div className="">
-          {currentPhrase.split('').map(
-            (letter) => 
+        <Timer 
+          className="timer" {...props}
+          secondsLeft={secondsLeft}
+          isRoundOver={isRoundOver}
+          roundOverMessage="Ronda terminada!"
+        />
+        <HangedMan {...props} hangedMan={game.hangedMan}/>
+        <div className="guess-phrase-container">
+          {game.phrases[currentPhraseIndex].split('').map(
+            (letter, i) => 
               letter === " "
-                ? <span className="whitespace">&nbsp;</span>
-                : <span className="letter">{letter}</span>
+                ? <span key={`ws-${i}`} className="whitespace">&nbsp;</span>
+                : <div key={`letter-${i}`} className="letter">
+                    <div className="character">{Object.keys(game.lettersPressed).includes(letter.toUpperCase()) ? letter.toUpperCase() : " "}</div>
+                    <hr className="underscore"/>
+                  </div>
           )}
         </div>
-        <Alphabet {...props}/>
+        <Alphabet
+          {...props}
+          lettersPressed={game.lettersPressed}
+          onLetterPressed={(letter) => onNewLetterPressed(letter)}
+        />
+        { isRoundOver &&
+          (<OverlayResult
+            {...props}
+            hasGuessed={hasGuessed}
+            phrase={game.phrases[currentPhraseIndex]}
+            isGameOver={isGameOver()}
+            onContinue={() => nextRound()}
+            onResetGame={() => resetGame()}
+          />
+        )}
+        
       </HangedGameContainer>
+      { gameMenuEnabled && (<GameSettings {...props} game={game} onUpdateGame={(updatedGame) => updateGameAndRestart(updatedGame)} setGameMenuEnabled={setGameMenuEnabled}/>) }
       <GameActions>
-        <ButtonAnt color="default" className="btn-action">Editar juego</ButtonAnt>
-        <ButtonAnt color="danger" className="btn-action">Saltar turno</ButtonAnt>
+        <ButtonAnt color="default" className="btn-action" onClick={() => setGameMenuEnabled(true)}>Editar juego</ButtonAnt>
+        <ButtonAnt color="danger" className="btn-action" onClick={() => nextRound()} disabled={isGameOver()}>Saltar turno</ButtonAnt>
       </GameActions>
     </>
   );
 };
 
+
 const GameActions = styled.div`
   display: flex;
   justify-content: space-between;
+  margin: 32px 8px 0 8px;
+  padding-bottom: 32px;
+
   .btn-action {
     display: inline-block;
+    font-weight: bold;
   }
 `;
 
 const HangedGameContainer = styled.div`
-  width: 100%;
-  height: calc(100vh - 50px);
-  .main-container {
-    .top-container-user {
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);
+  margin: 0 12px;
+  padding-top: 8px;
+
+  .guess-phrase-container {
+    text-align: center;
+    margin-bottom: 60px;
+
+    .whitespace {
+      margin: 7px;
+    }
+
+    .letter {
+      color: ${(props) => props.theme.basic.white};
+      font-family: Lato;
+      font-style: normal;
+      font-weight: bold;
+      font-size: 20px;
+      line-height: 28px;
+
+      margin: 8px 4px;
+      min-width: 28px;
+      display: inline-flex;
       align-items: center;
       justify-content: center;
-      margin: 1rem 0;
-      padding: 0.5rem;
-      .pattern {
-        background: ${(props) => props.theme.basic.secondary};
-        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
-        border-radius: 4px;
-        padding: 0.5rem 1rem;
-        margin: 0 auto;
-        max-width: 220px;
+      flex-wrap: wrap;
+      vertical-align: bottom;
+
+      .character {
+        text-transform: uppercase;
       }
-    }
-    .bingo-card-container {
-      margin: 0 auto;
-    }
-    .buttons-container {
-      margin: 1rem;
-      display: flex;
-      align-items: center;
-      justify-content: space-evenly;
-      align-items: center;
-      button {
+
+      .underscore {
         width: 100%;
-      }
-    }
-    .tablet-tabs {
-      height: 32px;
-      background: ${(props) => props.theme.basic.primary};
-      display: grid;
-      grid-template-columns: repeat(2, 1fr);
-      .tab {
-        padding: 0.5rem 1rem;
-        text-align: center;
-        font-family: "Encode Sans", sans-serif;
-        font-style: normal;
-        font-size: 15px;
-        font-weight: 400 !important;
-        line-height: 17px;
-        position: relative;
-        cursor: pointer;
-        color: ${(props) => props.theme.basic.secondary};
-      }
-      .active {
-        color: ${(props) => props.theme.basic.whiteLight};
-      }
-      .active::after {
-        content: "";
-        position: absolute;
-        bottom: 0;
-        left: 50%;
-        transform: translateX(-50%);
-        width: 80%;
-        height: 2px;
-        background: ${(props) => props.theme.basic.whiteLight};
-      }
-    }
-    .bingo-board {
-      margin: 1rem auto;
-      padding: 0.5rem;
-    }
-    .pattern-rounds {
-      display: grid;
-      align-items: center;
-      grid-template-columns: repeat(2, 50%);
-      margin: 1rem 0;
-      .left-container {
-        .card-pattern-container {
-          background: ${(props) => props.theme.basic.secondary};
-          box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
-          border-radius: 4px;
-          padding: 0.5rem 1rem;
-          margin: 0 auto;
-          max-width: 250px;
-        }
-      }
-      .right-container {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        padding: 0.5rem;
-        .last-plays {
-          width: 100%;
-          max-width: 200px;
-        }
-        .btns-container {
-          width: 100%;
-          button {
-            padding: 1rem;
-          }
-        }
-      }
-    }
-    .options-container {
-      margin: 1rem 0;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-    }
-  }
-  .awards {
-    font-family: Lato;
-    font-style: normal;
-    font-weight: bold;
-    font-size: 12px;
-    line-height: 14px;
-    text-decoration: underline;
-    color: ${(props) => props.theme.basic.primaryLight};
-    cursor: pointer;
-    padding: 1rem;
-    max-width: 430px;
-    margin: 0 auto;
-  }
-  .chat-container {
-    height: 550px;
-  }
-  .last-plays-container {
-    margin: 0.5rem auto;
-    overflow: auto;
-    max-width: 430px;
-  }
-  ${mediaQuery.afterTablet} {
-    display: grid;
-    grid-template-columns: calc(100% - 300px) 300px;
-    .main-container {
-      padding: 0;
-      overflow: auto;
-      .user-content {
-        display: grid;
-        padding: 0.5rem 0.5rem 2rem 0.5rem;
-        grid-template-columns: auto auto;
-        grid-gap: 1rem;
-        border-bottom: 10px solid ${(props) => props.theme.basic.primary};
-        overflow: auto;
-        .left-user-content {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-direction: column;
-          button {
-            width: 100%;
-            max-width: 350px;
-            margin: 1rem auto;
-            padding: 1rem;
-            font-size: 25px;
-            line-height: 30px;
-          }
-        }
-        .right-user-content {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-direction: column;
-          .board-container {
-            width: 100%;
-          }
-          .bottom-section {
-            display: grid;
-            grid-template-columns: auto minmax(250px, auto) minmax(200px, auto);
-            grid-gap: 1rem;
-            align-items: center;
-            width: 100%;
-            margin: 1rem 0;
-            .last-plays-container {
-              margin: 0;
-              min-width: 250px;
-            }
-            .pattern {
-              background: ${(props) => props.theme.basic.secondary};
-              box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
-              border-radius: 4px;
-              padding: 0.5rem 1rem;
-            }
-          }
-        }
-      }
-    }
-    .chat-container {
-      height: 100%;
-    }
-    .bingo {
-      padding: 0.5rem 0.5rem 2rem 0.5rem;
-      display: grid;
-      grid-template-columns: 250px auto;
-      border-bottom: 10px solid ${(props) => props.theme.basic.primary};
-      grid-gap: 2rem;
-      overflow: auto;
-      .left-container {
-        .card-pattern-container {
-          background: ${(props) => props.theme.basic.secondary};
-          box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.25);
-          padding: 1rem;
-          border-radius: 4px;
-        }
-      }
-      .right-container {
-        .board-container {
-          margin: 0;
-        }
-        .awards {
-          padding: 0;
-        }
-        .bottom-section {
-          display: grid;
-          grid-template-columns: repeat(3, 1fr);
-          grid-gap: 1rem;
-          align-items: center;
-          max-width: 800px;
-          margin: 1rem 0;
-        }
-      }
-      .subtitle {
-        font-family: Lato;
-        font-style: normal;
-        font-weight: bold;
-        font-size: 18px;
-        line-height: 22px;
-        color: ${(props) => props.theme.basic.whiteLight};
-        padding: 1rem;
       }
     }
   }
