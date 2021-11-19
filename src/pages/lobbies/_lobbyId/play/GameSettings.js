b17fe693a895a68258c69373be06f0fc8f6ad901
anargu <anargu@pop-os.localdomain>
2021-11-19 11:58:35 -0500

    refactor imports && code format

--- src/pages/lobbies/_lobbyId/play/GameSettings.js
+++ src/pages/lobbies/_lobbyId/play/GameSettings.js
@@ -1,20 +1,12 @@
-import React, { useEffect, useGlobal, useState } from "reactn";
+import React, { useGlobal, useState } from "reactn";
 import styled from "styled-components";
-import { Desktop, mediaQuery, Tablet } from "../../../../constants";
-import defaultTo from "lodash/defaultTo";
-import { UserLayout } from "../userLayout";
-import { firestore } from "../../../../firebase";
-import { HangedMan } from "./HangedMan";
-import { Timer } from "./Timer";
-import { Alphabet } from "./Alphabet";
-import { OverlayResult } from "./OverlayResult";
-import { ButtonAnt } from "../../../../components/form";
 import { GameMenu } from "../../../../components/GameMenu";
-import { useInterval } from "../../../../hooks/useInterval";
 
 export const GameSettings = (props) => {
   const [audios] = useGlobal("audios");
+
   const [isLoadingSave, setIsLoadingSave] = useState(false);
+
   let modifiedGame = { ...props.game }; 
 
  return (
